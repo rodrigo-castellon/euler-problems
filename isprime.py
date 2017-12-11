f3ba8a0a6d455f0f831a9e1ce8efb908dd0d02e9
Rodrigo Castellon <rodrigo.castellon@pinecrest.edu>
2017-12-10 20:03:30 -0500

    Delete isprime.py

--- isprime.py
+++
@@ -1,9 +0,0 @@
-def isprime(primenumber):
-    i = 2
-    while i < primenumber/2:
-        if primenumber % i == 0:
-            return False
-            break
-        i += 1
-    else:
-        return True
